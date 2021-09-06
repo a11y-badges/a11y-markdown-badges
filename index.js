346b116594d3b420b0d008a5a5dd5d68340f53f5
ctcpip <ctcpip@users.noreply.github.com>
2021-09-06 13:35:14 -0500

    🔨 refactor readme builder

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ const contrast = require("get-contrast");
 const simpleIcons = require('simple-icons');
 const fs = require('fs');
 
-const readme = [
-  '# a11y markdown badges'
-  ,''
-  ,'accessible badges for your profile `README` and projects via [shields.io](https://shields.io) and [Simple Icons](https://github.com/simple-icons/simple-icons)'
-  ,''
-  ,'## use'
-  ,''
-  ,'- copy the markdown snippet and paste it in your markdown file'
-  ,'- customize as you see fit - badge style, colors, etc. (see [shields.io](https://shields.io) for documentation)'
-  ,''
-  ,'## limitations'
-  ,''
-  ,'currently, we have no control over the font color, so we can still end up with badges of poor contrast.  there is [an open issue at shields.io](https://github.com/badges/shields/issues/5497) for this.'
-  ,'until that gets resolved, for any poor contrast badges, I suggest darkening the colors in the snippet until it meets WCAG AA.  although this sacrifices some brand identity, it is more important that we accommodate folks with visual issues.'
-  ,''
-  ,'## the badges'
-  ,''
-  ,'I suggest visiting the [README](README.md) file directly to better see the badges, as they should appear larger there.'
-  ,''
-  ,'| Name | Badge | Markdown Snippet |'
-  ,'| --- | --- | --- |'
-];
+const readmeStart =
+`# a11y markdown badges
+
+accessible badges for your profile \`README\` and projects via [shields.io](https://shields.io) and [Simple Icons](https://github.com/simple-icons/simple-icons)
+
+## use
+
+- copy the markdown snippet and paste it in your markdown file
+- customize as you see fit - badge style, colors, etc. (see [shields.io](https://shields.io) for documentation)
+
+## limitations
+
+currently, we have no control over the font color, so we can still end up with badges of poor contrast.  there is [an open issue at shields.io](https://github.com/badges/shields/issues/5497) for this.
+until that gets resolved, for any poor contrast badges, I suggest darkening the colors in the snippet until it meets WCAG AA.  although this sacrifices some brand identity, it is more important that we accommodate folks with visual issues.
+
+## the badges
+
+I suggest visiting the [README](README.md) file directly to better see the badges, as they should appear larger there.
+
+| Name | Badge | Markdown Snippet |
+| --- | --- | --- |
+`;
+
+const badgeLines = [];
 
 for (const iconSlug in simpleIcons) {
 
@@ -44,22 +46,24 @@ for (const iconSlug in simpleIcons) {
   const badgeURL = `https://img.shields.io/badge/${icon.title.replace(/ /g,'_').replace(/-/g,'--')}-${icon.hex}.svg?style=for-the-badge&logo=${icon.slug}&logoColor=${fgColor}`
   const badgeLine = `|${icon.title}|![${icon.title}](${badgeURL})|\`![${icon.title}](${badgeURL})\`|`;
 
-  readme.push(badgeLine);
+  badgeLines.push(badgeLine);
 
 }
 
-readme.push(
-    ''
-    ,'## implementation details'
-    ,''
-    ,'- iterates over all [Simple Icons](https://github.com/simple-icons/simple-icons)'
-    ,'- checks if a white foreground meets WCAG AA contrast requirements for accessibility (a11y)'
-    ,'- if white does not provide accessible contrast for the background color, it uses black (and confirms black provides accessible contrast)'
-    ,'- generates this file'
-    ,''
-    ,'## inspiration'
-    ,''
-    ,'inspired by [Markdown Badges](https://github.com/Ileriayo/markdown-badges), I wanted an automated way to generate accessible contrast versions of all the badges'
-)
+const readmeEnd =
+`
+## implementation details
 
-fs.writeFileSync("README.md", readme.join('\n'));
+- iterates over all [Simple Icons](https://github.com/simple-icons/simple-icons)
+- checks if a white foreground meets WCAG AA contrast requirements for accessibility (a11y)
+- if white does not provide accessible contrast for the background color, it uses black (and confirms black provides accessible contrast)
+- generates this file
+
+## inspiration
+
+inspired by [Markdown Badges](https://github.com/Ileriayo/markdown-badges), I wanted an automated way to generate accessible contrast versions of all the badges
+`;
+
+const readme = readmeStart + badgeLines.join('\n') + readmeEnd;
+
+fs.writeFileSync("README.md", readme);
